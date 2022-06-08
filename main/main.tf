6391a6cc7fd6f18e3bdf49f53731838cdfc87659
Guravaiah Ganneboina <guru@guravaiahs-air.home>
2022-06-08 22:35:34 +0100

    i created a new one

--- main/main.tf
+++ main/main.tf
@@ -2,7 +2,8 @@ provider "azurerm" {
   features {}
 }
 module "resource_group" {
-  source = "../modules/rg"
+  source  = "../modules/rg"
+  rg_name = var.rg_name
 }
 module "storage_account" {
   source              = "../modules/st"
